a7b40e1185e60c016736404b9ea5139399eff68c
marqueane <marqueane_64@hotmail.com>
2023-10-11 18:50:16 -0300

    Criado template indice.html.

--- app_aperitivos/urls.py
+++ app_aperitivos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from app_aperitivos.views import video
+from app_aperitivos.views import video, indice
 
 app_name='app_aperitivos'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
+    path('', indice, name='indice'),
 ]
--- app_aperitivos/views.py
+++ app_aperitivos/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 737734204},
+    # se eu controlo os títulos e os ids respectivos, eu pdoerei usar o template para qualquer vídeo.
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 251497668},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+def indice(request):
+    return render(request, 'app_aperitivos/indice.html', context={'videos': videos})
+
+
 def video(request, slug):
-    return render(request, 'app_aperitivos/video.html')
+    video = videos_dct[slug]
+    return render(request, 'app_aperitivos/video.html', context={'video': video})
